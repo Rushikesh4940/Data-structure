7a2fcdbf8b15fc073f301c6b125a86392d32d84b
Rushikesh Gund <94077582+Rushikesh4940@users.noreply.github.com>
2023-07-10 14:25:50 +0530

    Add files via upload

---
+++ Math/NumberTheoery.java
@@ -0,0 +1,37 @@
+public class NumberTheoery {
+    public static void main(String[] args) {
+        printBinary(199);
+        printBinary(setittosetbit(199, 4));
+        printBinary(setittounset(199, 4));
+        oddoreven(28);
+        System.out.println(uppertolower('A'));
+        System.out.println(lowertoupper('a'));
+    }    
+    public static void printBinary(int value) {
+    while(value>0){
+        System.out.print(value&1);
+        value=value>>1;
+    }
+    System.out.println();
+    } 
+    public static int setittosetbit(int n,int pos) {
+        return n|1<<pos;
+    }
+    public static int setittounset(int n,int pos) {
+    return n&~(1<<pos);
+    }
+    public  static void oddoreven(int n){
+        if((n&1)==0){
+            System.out.println("even");
+        }
+        else{
+            System.out.println("odd"); 
+        }
+    }
+    public static char lowertoupper(char c){
+        return (char)(c&'_') ;
+    }
+    public static char uppertolower(char c){
+             return (char)(c|' ') ;
+    }
+}
